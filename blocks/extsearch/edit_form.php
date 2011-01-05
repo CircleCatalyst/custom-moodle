99e800b7a858e89cbe951ca8d6e25b398df1b014
Aaron Wells <aaronw@catalyst.net.nz>
2011-01-05 16:32:42 +1300

    blocks/extsearch/edit_form.php Converting to proper Moodle 2 edit form
    WR76545

--- blocks/extsearch/edit_form.php
+++ blocks/extsearch/edit_form.php
@@ -1,87 +1,46 @@
 <?php
-$id = optional_param('id', SITEID, PARAM_INT);
-print_box_start();
-?>
 
-<table cellpadding="9" cellspacing="0" class="blockconfigtable">
-<tr valign="top">
-    <td class="label">
-        <?php print_string('searchprovider_label', 'block_extsearch') ?>
-    </td>
-    <td class="value">
-<?php
+class block_extsearch_edit_form extends block_edit_form {
 
-$options['google'] = get_string('google', 'block_extsearch');
-$selected = 'google';
+    /**
+     * Adds form fields specific to this block
+     *
+     * @param moodleform $mform
+     */
+    protected function specific_definition($mform){
+        global $CFG;
 
-$digitalnzapikey = get_config(NULL, 'block_extsearch_digitalnz_api_key');
-if (!empty($digitalnzapikey)) {
-    $options['digitalnz'] = get_string('digitalnz', 'block_extsearch');
-    $options['edna'] = get_string('edna', 'block_extsearch');
-    $selected = 'digitalnz';
-}
+        // Figure out the options for the search provider dropdown menu
+        $options = array();
+        $options['google'] = get_string('google', 'block_extsearch');
+        $default = 'google';
 
-$esfsclienttoken = get_config(NULL, 'block_extsearch_esfs_client_token');
-if (!empty($esfsclienttoken)) {
-    $options['esfs'] = get_string('esfs', 'block_extsearch');
-    $selected = 'esfs';
-}
+        $digitalnzapikey = get_config(NULL, 'block_extsearch_digitalnz_api_key');
+        if (!empty($digitalnzapikey)) {
+            $options['digitalnz'] = get_string('digitalnz', 'block_extsearch');
+            $options['edna'] = get_string('edna', 'block_extsearch');
+            $default = 'digitalnz';
+        }
 
-if (isset($options)) {
-    if (isset($this->config) && !empty($this->config->search_provider)) {
-        $selected = $this->config->search_provider;
+        $esfsclienttoken = get_config(NULL, 'block_extsearch_esfs_client_token');
+        if (!empty($esfsclienttoken)) {
+            $options['esfs'] = get_string('esfs', 'block_extsearch');
+            $default = 'esfs';
+        }
+        $mform->addElement('select', 'config_search_provider', get_string('searchprovider_label','block_extsearch'), $options);
+        $mform->setDefault('config_search_provider', $default);
+
+        // Figure out the options for the Google SafeSearch dropdown menu
+        $safesearch = array();
+        $safesearch['active'] = get_string('googlesafesearch_active', 'block_extsearch');
+        $safesearch['moderate'] = get_string('googlesafesearch_moderate', 'block_extsearch');
+        $safesearch['off'] = get_string('googlesafesearch_off', 'block_extsearch');
+        $mform->addElement('select', 'config_google_safesearch', get_string('googlesafesearch_label','block_extsearch'), $safesearch);
+        $mform->setDefault('config_google_safesearch','moderate');
+
+        $mform->addElement('checkbox', 'config_popup_links', get_string('popuplinks', 'block_extsearch'));
+
+        $mform->addElement('html', '<i>'.get_string('noteaboutsitewideconfig', 'block_extsearch', $CFG->wwwroot.'/admin/settings.php?section=blocksettingextsearch').'<i>');
     }
-    choose_from_menu($options, 'search_provider', $selected);
+
 }
-else {
-    print_string('noneavailable', 'block_extsearch');
-    echo '<input type="hidden" name="search_provider" value="" />';
-}
-
-?>
-    </td>
-</tr>
-
-<tr valign="top">
-    <td class="label">
-        <?php print_string('googlesafesearch_label', 'block_extsearch') ?>
-    </td>
-    <td class="value">
-<?php
-
-$safesearch['active'] = get_string('googlesafesearch_active', 'block_extsearch');
-$safesearch['moderate'] = get_string('googlesafesearch_moderate', 'block_extsearch');
-$safesearch['off'] = get_string('googlesafesearch_off', 'block_extsearch');
-
-$selected = 'moderate';
-if (isset($this->config) && !empty($this->config->google_safesearch)) {
-    $selected = $this->config->google_safesearch;
-}
-
-choose_from_menu($safesearch, 'google_safesearch', $selected);
-
-?>
-    </td>
-</tr>
-<tr>
-    <td colspan="2" class="submit">
-<?php
-    $checked = '';
-    if (isset($this->config) && !empty($this->config->popup_links)) {
-        $checked = 'checked="checked"';
-    }
-?>
-        <label for="popup_links"><?php print_string('popuplinks', 'block_extsearch') ?></label>
-        <input type="checkbox" name="popup_links" id="popup_links" value="1" <?php print $checked ?> />
-    </td>
-</tr>
-<tr>
-    <td colspan="2" class="submit">
-        <input type="submit" value="<?php print_string('savechanges') ?>" />
-    </td>
-</tr>
-</table>
-
-<p><i><?php print get_string('noteaboutsitewideconfig', 'block_extsearch', $CFG->wwwroot .'/admin/settings.php?section=blocksettingextsearch'); ?></i></p>
-
-<?php print_box_end(); ?>
