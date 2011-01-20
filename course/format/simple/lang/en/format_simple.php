c83363bdf692f20292aa3d6b3fda55a9c42b11b5
Aaron Wells <aaronw@catalyst.net.nz>
2011-01-20 17:43:23 +1300

    course/format/simple: First crack at simple course format in Moodle 2
    Works except for hide/show blocks

---
+++ course/format/simple/lang/en/format_simple.php
@@ -0,0 +1,7 @@
+<?php
+$string['sectionname']='Topic'; // Name of a section within your format
+$string['pluginname']='Simple topics format';
+
+$string['hideblocks'] = 'Hide Blocks';
+$string['showblocks'] = 'Show Blocks';
+?>
