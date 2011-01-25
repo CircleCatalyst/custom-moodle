40f623efbb1c8b9127687bc900d6c0d938dd08ea
Aaron Wells <aaronw@catalyst.net.nz>
2011-01-25 20:43:53 +1300

    local/nzschools/profiles: Commenting out demo course creation for now

--- local/nzschools/profiles/primary/upgrade.php
+++ local/nzschools/profiles/primary/upgrade.php
@@ -25,9 +25,10 @@ function nzschoolsprofile_upgrade_primary($oldversion) {
         // Setup course categories
         if (!empty($createcats)) {
             local_createcats($fromyear, $toyear);
-            if ( isset($CFG->templatecat) ){
-                local_restoretemplates($CFG->dirroot.'/local/nzschools/profiles/primary/templatecourses');
-            }
+            // todo: Uncomment this once a Moodle 2 version of import_backup_file_silently() is implemented
+//            if ( isset($CFG->templatecat) ){
+//                local_restoretemplates($CFG->dirroot.'/local/nzschools/profiles/primary/templatecourses');
+//            }
         }
 
         // Configure Front page
