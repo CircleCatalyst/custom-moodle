6e1ba1d52c8211005ce88d47a59174b4385be0cc
Aaron Wells <aaronw@catalyst.net.nz>
2011-01-25 15:25:23 +1300

    local/nzschools/logo.php Setting default logo to one w/transparent bg

--- local/nzschools/logo.php
+++ local/nzschools/logo.php
@@ -1,10 +1,14 @@
 <?php
 
+// try to suppress error notices (since they'll break the image)
+define('NO_DEBUG_DISPLAY', true);
+
 require('../../config.php');
 require_once($CFG->libdir.'/filelib.php');
 
-// disable moodle specific debug messages
-define('NO_DEBUG_DISPLAY', true);
+// try to suppress error notices (since they'll break the image)
+$CFG->debudisplay = 0;
+
 $force = optional_param('force',false,PARAM_BOOL);
 $fs = get_file_storage();
 $context = get_context_instance(CONTEXT_SYSTEM);
@@ -15,9 +19,8 @@ if ( count($files) ){
 	send_stored_file($file);
 } elseif ($force){
 	die();
-} elseif (is_file($CFG->dirroot.'/theme/'.current_theme().'/images/logo.png')) {
-    send_file($CFG->dirroot.'/theme/'.current_theme().'/images/logo.png', 'logo.png', 525600);
+} elseif (is_file($CFG->dirroot.'/theme/'.current_theme().'/pix/logo.png')) {
+    send_file($CFG->dirroot.'/theme/'.current_theme().'/pix/logo.png', 'logo.png', 525600);
 } else {
     send_file($CFG->dirroot.'/pix/moodlelogo-med-white.gif', 'moodlelogo-med-white.gif', 525600);
 }
-?>
