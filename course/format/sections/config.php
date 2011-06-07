afae3e8389df0a6f9c5b277773539e8de0511e92
christopher tombleson <christopher@catalyst.net.nz>
2011-06-08 11:40:15 +1200

    WR#81499 course/format/sections/config.php: removed admin from default block list

--- course/format/sections/config.php
+++ course/format/sections/config.php
@@ -2,11 +2,11 @@
 //
 // Optional course format configuration file
 //
-// This file contains any specific configuration settings for the 
+// This file contains any specific configuration settings for the
 // format.
 //
 // The default blocks layout for this course format:
     $format['defaultblocks'] = 'menu_site_and_course,activity_modules,search_forums,'.
-                               'admin,course_list:news_items,calendar_month,calendar_upcoming,'.
+                               'course_list:news_items,calendar_month,calendar_upcoming,'.
                                'recent_activity';
 //
