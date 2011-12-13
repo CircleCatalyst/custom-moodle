a96868e674c8fddfb2d7a67c994705e502ccf399
Hugh Davenport <hugh@catalyst.net.nz>
2011-12-14 10:40:27 +1300

    mod/hotpot: Fix bug with attempts

--- mod/hotpot/attempt/html/renderer.php
+++ mod/hotpot/attempt/html/renderer.php
@@ -233,7 +233,7 @@ class mod_hotpot_attempt_html_renderer extends mod_hotpot_attempt_renderer {
 
         list($select, $params) = $DB->get_in_or_equal($urls);
         $select = "course=? AND sourcefile $select";
-        $params = array_unshift($params, $this->hotpot->course->id);
+        array_unshift($params, $this->hotpot->course->id);
 
         if ($quizzes = $DB->get_records_select('hotpot', $select, $params, 'id', 'id,sourcefile')) {
             foreach ($quizzes as $quiz) {
