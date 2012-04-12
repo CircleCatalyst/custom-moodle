3568541193853efdae3c32e7e2b5590eedbffb82
Bart Kamphuis <bart.kamphuis@watchdog.net.nz>
2012-04-12 16:22:19 +1200

    corrected topcoll plugin files to use from correct git repo branch

--- course/format/topcoll/version.php
+++ course/format/topcoll/version.php
@@ -29,8 +29,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-$plugin->version = 2012031700;
+$plugin->version = 2012032100;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2012031500.00; // 2.3dev
+$plugin->requires = 2011120500.00; // 2.2
 $plugin->component = 'format_topcoll';
-$plugin->release = '2.3.4.1';
+$plugin->release = '2.2.4.2';
